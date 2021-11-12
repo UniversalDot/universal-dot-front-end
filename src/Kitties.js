cf78d908875b9ff5e5c9cf3a12af9ef198c181d1
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-11-12 11:25:06 +0100

    add structs

--- src/Kitties.js
+++ src/Kitties.js
@@ -5,3 +5,27 @@ import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
 
 import KittyCards from './KittyCards';
+
+
+// Construct a Kitty ID from storage key
+const convertToKittyHash = entry =>
+  `0x${entry[0].toJSON().slice(-64)}`;
+
+// Construct a Kitty object
+const constructKitty = (hash, { dna, price, gender, owner }) => ({
+  id: hash,
+  dna,
+  price: price.toJSON(),
+  gender: gender.toJSON(),
+  owner: owner.toJSON()
+});
+
+// Use React hooks
+export default function Kitties (props) {
+  const { api, keyring } = useSubstrate();
+  const { accountPair } = props;
+
+  const [kittyHashes, setKittyHashes] = useState([]);
+  const [kitties, setKitties] = useState([]);
+  const [status, setStatus] = useState('');
+// snip
