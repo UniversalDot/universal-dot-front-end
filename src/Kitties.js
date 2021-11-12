8896cd431fd59d512ed19fff4fc1528be520745a
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-11-12 11:19:05 +0100

    add file

---
+++ src/Kitties.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { Form, Grid } from 'semantic-ui-react';
+
+import { useSubstrate } from './substrate-lib';
+import { TxButton } from './substrate-lib/components';
+
+import KittyCards from './KittyCards';
