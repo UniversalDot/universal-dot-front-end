e252b0a4e3dc3f2a8235ffa52d275bbd00c6fe5c
Atanasov, Slave <slave.atanasov@ext.illimity.com>
2022-01-13 19:21:48 +0100

    init add kanban and calendar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
+import './styles/styles.scss';
 
 import { SubstrateContextProvider } from './substrate-lib';
 
--- src/pages/organization/Organization.jsx
+++ src/pages/organization/Organization.jsx
@@ -1,7 +1,59 @@
 import React from 'react';
+import Board from 'react-trello';
 
 const Organization = () => {
-  return <h1>Organization page</h1>;
+  const mockData = {
+    lanes: [
+      {
+        id: 'lane1',
+        title: 'Backlog',
+        label: '2',
+        cards: [
+          {
+            id: 'Card1',
+            title: 'Write Blog',
+            description: 'Can AI make memes',
+            label: '30 mins',
+            draggable: false,
+          },
+          {
+            id: 'Card2',
+            title: 'Pay Rent',
+            description: 'Transfer via NEFT',
+            label: '5 mins',
+            metadata: { sha: 'be312a1' },
+          },
+        ],
+      },
+      {
+        id: 'lane2',
+        title: 'To Do',
+        label: '0',
+        cards: [],
+      },
+      {
+        id: 'lane3',
+        title: 'In Progress',
+        label: '0',
+        cards: [],
+      },
+      {
+        id: 'lane4',
+        title: 'Done',
+        label: '0',
+        cards: [],
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Board
+        data={mockData}
+        style={{ height: '50vh', background: 'none', padding: '0' }}
+      />
+    </>
+  );
 };
 
 Organization.displayName = 'Organization';
