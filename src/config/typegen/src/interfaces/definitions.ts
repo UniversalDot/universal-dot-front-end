80c7cfa055d51dcfaf1ef6bde801e308860275d8
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2022-01-24 11:00:39 +0100

    export type definitions

---
+++ src/config/typegen/src/interfaces/definitions.ts
@@ -0,0 +1,7 @@
+// order here matches with what is found in (unused removed)
+//   https://www.npmjs.com/package/edgeware-node-types
+//   https://github.com/hicommonwealth/edgeware-node-types/tree/master/types
+
+export { default as profile } from './profile/definitions';
+export { default as task } from './task/definitions';
+export { default as dao } from './dao/definitions';
