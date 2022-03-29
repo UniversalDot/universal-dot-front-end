ead1848c57db6228f03fb22271de553d6dbb9105
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2022-03-29 12:21:07 +0200

    add selectors to organizations

--- src/components/organizations/Organizations.jsx
+++ src/components/organizations/Organizations.jsx
@@ -335,6 +335,7 @@ const Organizations = ({ type }) => {
               <Button
                 color="green"
                 type="submit"
+                data-cy='createOrganization'
                 onClick={() => buttonClick('own_createOrganization')}
               >
                 Create organization
@@ -344,6 +345,7 @@ const Organizations = ({ type }) => {
               <Button
                 color="green"
                 type="submit"
+                data-cy='addMember'
                 onClick={() => buttonClick('own_addMember')}
               >
                 Add member
@@ -371,6 +373,7 @@ const Organizations = ({ type }) => {
             <Grid.Column className={styles.column}>
               <Button
                 color="orange"
+                data-cy='dissolveOrganization'
                 onClick={() => buttonClick('own_dissolveOrganization')}
               >
                 Dissolve organization
@@ -457,6 +460,7 @@ const Organizations = ({ type }) => {
                   fluid
                   type="text"
                   label="Organization name:"
+                  data-cy="modalOrganizationName"
                   value={organizationNameForAction || ''}
                   onChange={e =>
                     handleTopInputOnChange(daoType, e.target.value)
@@ -472,6 +476,7 @@ const Organizations = ({ type }) => {
                   placeholder="Enter value..."
                   fluid
                   type="text"
+                  data-cy='modalTaskMember'
                   label={taskTypes.includes(daoType) ? 'Task:' : 'Member:'}
                   value={memberOrTaskForAction || ''}
                   onChange={e =>
